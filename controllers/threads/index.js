6c74370ac1e35c685ef1c85437c13a8cb311d8a2
zaichaopan <zaichaopan@gmail.com>
2018-01-15 09:51:53 -0800

    Format threads controller

--- controllers/threads/index.js
+++ controllers/threads/index.js
@@ -2,12 +2,8 @@ const Thread = require('../../models/thread');
 const asyncWrapper = require('../../helper/asyncWrapper');
 
 const findThread = async(req, next) => {
-    const {
-        _id
-    } = req.params;
-    const thread = await Thread.findOne({
-        _id
-    });
+    const { _id } = req.params;
+    const thread = await Thread.findOne({ _id });
     if (!thread) next('route');
     req.thread = thread;
     next();
@@ -25,47 +21,19 @@ exports.before = {
 exports.index = asyncWrapper(async(req, res, next) => {
     const threads = await Thread.find({});
     res.format({
-        html() {
-            res.render('threads/index', {
-                threads
-            });
-        },
-        json() {
-            res.send({
-                threads
-            });
-        }
+        html() { res.render('threads/index', { threads }); },
+        json() { res.send({ threads }); }
     });
 });
 
-exports.show = (req, res, next) => {
-    res.format({
-        html() {
-            res.render('threads/index', {
-                thread: req.thread
-            });
-        },
-        json() {
-            res.send({
-                thread: req.thread
-            });
-        }
-    });
-};
+exports.show = (req, res, next) => res.format({
+    html() { res.render('threads/index', { thread: req.thread }); },
+    json() { res.send({ thread: req.thread }); }
+});
 
 exports.create = (req, res, next) => res.render('threads.create');
 
-exports.edit = (req, res, next) => {
-    res.format({
-        html() {
-            res.render('threads/edit', {
-                thread: req.thread
-            });
-        },
-        json() {
-            res.send({
-                thread: req.thread
-            });
-        }
-    });
-}
+exports.edit = (req, res, next) => res.format({
+    html() { res.render('threads/edit', { thread: req.thread }); },
+    json() { res.send({ thread: req.thread }); }
+});
